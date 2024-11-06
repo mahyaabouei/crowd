da4bea273d3ec8ab6a422c74ce43013f2070feff
Mahya Abouei <mahyaaboueii@gmail.com>
2024-11-06 14:59:29 +0330

    payment limit

--- plan/views.py
+++ plan/views.py
@@ -604,38 +604,38 @@ class PaymentDocument(APIView):
         plan = Plan.objects.filter(trace_code=trace_code).first()
         if not plan:
             return Response({'error': 'plan not found'}, status=status.HTTP_404_NOT_FOUND)
-        information_plan = InformationPlan.objects.filter(plan=plan).first()
+        # information_plan = InformationPlan.objects.filter(plan=plan).first()
         if not request.data.get('amount'):
             return Response({'error': 'amount not found'}, status=status.HTTP_404_NOT_FOUND)
         amount = int(request.data.get('amount')) # سهم درخواستی کاربر 
-        amount_collected_now = information_plan.amount_collected_now # مبلغ جمه اوری شده تا به  الان
-        plan_total_price = plan.total_units # کل سهم قابل عرضه برای طرح 
-        purchaseable_amount = int(plan_total_price - amount_collected_now) # مبلغ قابل خرید همه کاربران 
-        if amount > purchaseable_amount :
-            return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
+        # amount_collected_now = information_plan.amount_collected_now # مبلغ جمه اوری شده تا به  الان
+        # plan_total_price = plan.total_units # کل سهم قابل عرضه برای طرح 
+        # purchaseable_amount = int(plan_total_price - amount_collected_now) # مبلغ قابل خرید همه کاربران 
+        # if amount > purchaseable_amount :
+        #     return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
         
-        if legal_user == True : 
-            amount_legal_min = plan.legal_person_minimum_availabe_price #حداقل سهم قابل خرید حقوقی 
-            amount_legal_max = plan.legal_person_maximum_availabe_price #حداکثر سهم قابل خرید حقوقی
+        # if legal_user == True : 
+        #     amount_legal_min = plan.legal_person_minimum_availabe_price #حداقل سهم قابل خرید حقوقی 
+        #     amount_legal_max = plan.legal_person_maximum_availabe_price #حداکثر سهم قابل خرید حقوقی
             
-            if amount_legal_min is not None and amount_legal_max is not None :
-                if amount < amount_legal_min or amount > amount_legal_max:
-                    return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
-            else :
-                if amount > purchaseable_amount :
-                    return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
+        #     if amount_legal_min is not None and amount_legal_max is not None :
+        #         if amount < amount_legal_min or amount > amount_legal_max:
+        #             return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
+        #     else :
+        #         if amount > purchaseable_amount :
+        #             return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
                   
                 
-        if legal_user == False :
-            amount_personal_min = plan.real_person_minimum_availabe_price  #حداقل سهم قابل خرید حقیقی
-            amount_personal_max = plan.real_person_maximum_available_price #حداکثر سهم قابل خرید حقیقی
-            if amount_personal_min is not None and amount_personal_max is not None:
-                if amount < amount_personal_min or amount > amount_personal_max :
-                    return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
+        # if legal_user == False :
+        #     amount_personal_min = plan.real_person_minimum_availabe_price  #حداقل سهم قابل خرید حقیقی
+        #     amount_personal_max = plan.real_person_maximum_available_price #حداکثر سهم قابل خرید حقیقی
+        #     if amount_personal_min is not None and amount_personal_max is not None:
+        #         if amount < amount_personal_min or amount > amount_personal_max :
+        #             return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
             
-            else :
-                if amount > purchaseable_amount :
-                    return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
+        #     else :
+        #         if amount > purchaseable_amount :
+        #             return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
                   
 
 
@@ -1260,7 +1260,7 @@ class WarrantyAdminViewset(APIView) :
         return Response(True , status=status.HTTP_200_OK)
 
 # done
-# فیش بانکی
+# درگاه بانکی
 # محدودیت پرداخت به دلیل امنیت غیر فعال شد
 class TransmissionViewset(APIView) : 
     @method_decorator(ratelimit(key='ip', rate='5/m', method='POST', block=True))
@@ -1273,42 +1273,42 @@ class TransmissionViewset(APIView) :
         if not user:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
         user = user.first()
-        legal_user = check_legal_person(user.uniqueIdentifier)
+        # legal_user = check_legal_person(user.uniqueIdentifier)
 
         plan = Plan.objects.filter(trace_code=trace_code).first()
-        information_plan = InformationPlan.objects.filter(plan=plan).first()
+        # information_plan = InformationPlan.objects.filter(plan=plan).first()
 
 
         value = request.data.get('amount')  # مبلغ درخواستی کاربر برای خرید 
-        amount_collected_now = information_plan.amount_collected_now # مبلغ جمه اوری شده تا به  الان
-        plan_total_price = plan.total_units # کل سهم قابل عرضه برای طرح 
-        purchaseable_amount = int(plan_total_price - amount_collected_now) # مبلغ قابل خرید همه کاربران 
+        # amount_collected_now = information_plan.amount_collected_now # مبلغ جمه اوری شده تا به  الان
+        # plan_total_price = plan.total_units # کل سهم قابل عرضه برای طرح 
+        # purchaseable_amount = int(plan_total_price - amount_collected_now) # مبلغ قابل خرید همه کاربران 
 
-        if value > purchaseable_amount :
-            return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
+        # if value > purchaseable_amount :
+        #     return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
         
-        if legal_user == True : 
-            amount_legal_min = plan.legal_person_minimum_availabe_price #حداقل سهم قابل خرید حقوقی 
-            amount_legal_max = plan.legal_person_maximum_availabe_price #حداکثر سهم قابل خرید حقوقی
+        # if legal_user == True : 
+        #     amount_legal_min = plan.legal_person_minimum_availabe_price #حداقل سهم قابل خرید حقوقی 
+        #     amount_legal_max = plan.legal_person_maximum_availabe_price #حداکثر سهم قابل خرید حقوقی
             
-            if amount_legal_min is not None and amount_legal_max is not None :
-                if value < amount_legal_min or value > amount_legal_max:
-                    return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
-            else :
-                if value > purchaseable_amount :
-                    return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
+        #     if amount_legal_min is not None and amount_legal_max is not None :
+        #         if value < amount_legal_min or value > amount_legal_max:
+        #             return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
+        #     else :
+        #         if value > purchaseable_amount :
+        #             return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
                   
                 
-        else :
-            amount_personal_min = plan.real_person_minimum_availabe_price  #حداقل سهم قابل خرید حقیقی
-            amount_personal_max = plan.real_person_maximum_available_price #حداکثر سهم قابل خرید حقیقی
-            if amount_personal_min is not None and amount_personal_max is not None :
-                if value < amount_personal_min or value > amount_personal_max :
-                    return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
+        # else :
+        #     amount_personal_min = plan.real_person_minimum_availabe_price  #حداقل سهم قابل خرید حقیقی
+        #     amount_personal_max = plan.real_person_maximum_available_price #حداکثر سهم قابل خرید حقیقی
+        #     if amount_personal_min is not None and amount_personal_max is not None :
+        #         if value < amount_personal_min or value > amount_personal_max :
+        #             return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
             
-            else :
-                if value > purchaseable_amount :
-                    return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
+        #     else :
+        #         if value > purchaseable_amount :
+        #             return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
                   
 
             

630c4026f4e702193b897266b10c7bcdd78a12f9
Mahya Abouei <mahyaaboueii@gmail.com>
2024-11-10 09:59:41 +0330

    reports

--- reports/views.py
+++ reports/views.py
@@ -15,7 +15,6 @@ from django.db.models import Sum
 import pandas as pd
 import datetime
 from persiantools.jdatetime import JalaliDate
-from authentication.serializers import accountsSerializer , privatePersonSerializer
 from plan.views import get_name , get_account_number
 import os
 from django.conf import settings
@@ -25,11 +24,42 @@ from django_ratelimit.decorators import ratelimit
 from django.utils.decorators import method_decorator
 
 
-# گزارش پیشرفت پروژه
-# done
-class ProgressReportViewset(APIView) :
-    @method_decorator(ratelimit(key='ip', rate='5/m', method='POST', block=True))
-    def post (self,request,trace_code) :
+
+class ProgressReportViewset(APIView):
+    """
+    This view allows an admin to upload a progress report associated with a specific plan, identified by its trace code.
+    """
+
+    @method_decorator(ratelimit(key='ip', rate='20/m', method='POST', block=True))
+    def post(self, request, trace_code):
+        """
+        Upload a progress report for a specific plan.
+
+        This method authenticates the admin using the Authorization header and allows them to upload a 
+        progress report associated with a plan identified by its trace code.
+
+        - If the Authorization header is missing, an error is returned.
+        - If the admin's authorization token is invalid or the admin is not found, an error is returned.
+        - If the plan with the specified trace code does not exist, an error is returned.
+        - If the request data or file upload is invalid, an error is returned.
+
+        Parameters:
+            - Authorization (str, header): Admin's authorization token.
+            - trace_code (str, path): Unique trace code identifier for the plan.
+            - report_data (dict, body): Data fields for the progress report.
+            - file (file, body, optional): File to be attached to the progress report.
+
+        Responses:
+            200: {
+                "id": <report_id>,
+                "plan": <plan_id>,
+                "uploaded_file": <file_url>,
+                ...
+            }
+            400: {"error": "Authorization header is missing" / "Invalid data"}
+            401: {"error": "admin not found"}
+            404: {"error": "Plan not found"}
+        """
         Authorization = request.headers.get('Authorization')
         if not Authorization:
             return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
@@ -53,6 +83,29 @@ class ProgressReportViewset(APIView) :
 
     @method_decorator(ratelimit(key='ip', rate='5/m', method='GET', block=True))
     def get (self,request,trace_code) :
+        """
+        Retrieve all progress reports for a specific plan by trace code.
+
+        This method fetches all progress reports associated with a plan identified by its trace code.
+
+        - If the plan with the specified trace code does not exist, an error is returned.
+        - If no progress reports are associated with the plan, an error is returned.
+
+        Parameters:
+            - trace_code (str, path): Unique trace code identifier for the plan.
+
+        Responses:
+            200: [
+                {
+                    "id": <report_id>,
+                    "plan": <plan_id>,
+                    "uploaded_file": <file_url>,
+                    ...
+                },
+                ...
+            ]
+            404: {"error": "Plan not found" / "progress report not found"}
+        """
         plan = Plan.objects.filter(trace_code=trace_code).first()
         if not plan:
             return Response({'error': 'Plan not found'}, status=status.HTTP_404_NOT_FOUND)
@@ -79,11 +132,40 @@ class ProgressReportViewset(APIView) :
         return Response({'message':'succes'} , status=status.HTTP_200_OK)
 
 
-# گزارش حسابررسی
-# done
-class AuditReportViewset(APIView) :
+class AuditReportViewset(APIView):
+    """
+    This view allows an admin to upload an audit report associated with a specific plan, identified by its trace code.
+    """
+
     @method_decorator(ratelimit(key='ip', rate='5/m', method='POST', block=True))
-    def post (self,request,trace_code) :
+    def post(self, request, trace_code):
+        """
+        Upload an audit report for a specific plan.
+
+        This method authenticates the admin using the Authorization header and allows them to upload an 
+        audit report associated with a plan identified by its trace code.
+
+        - If the Authorization header is missing, an error is returned.
+        - If the admin's authorization token is invalid or the admin is not found, an error is returned.
+        - If the plan with the specified trace code does not exist, an error is returned.
+        - If the request data or file upload is invalid, an error is returned.
+
+        Parameters:
+            - Authorization (str, header): Admin's authorization token.
+            - trace_code (str, path): Unique trace code identifier for the plan.
+            - audit_data (dict, body): Data fields for the audit report.
+            - file (file, body, optional): File to be attached to the audit report.
+
+        Responses:
+            200: {
+                "id": <report_id>,
+                "plan": <plan_id>,
+                "uploaded_file": <file_url>,
+                ...
+            }
+            400: {"error": "Authorization header is missing" / "Invalid data"}
+            404: {"error": "admin not found" / "Plan not found"}
+        """
         Authorization = request.headers.get('Authorization')
         if not Authorization:
             return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
@@ -104,9 +186,31 @@ class AuditReportViewset(APIView) :
         return Response (serializer.data, status=status.HTTP_200_OK)
     
 
-
     @method_decorator(ratelimit(key='ip', rate='5/m', method='GET', block=True))
     def get (self,request,trace_code) :
+        """
+        Retrieve all audit reports for a specific plan by trace code.
+
+        This method fetches all audit reports associated with a plan identified by its trace code.
+
+        - If the plan with the specified trace code does not exist, an error is returned.
+        - If no audit reports are associated with the plan, an error is returned.
+
+        Parameters:
+            - trace_code (str, path): Unique trace code identifier for the plan.
+
+        Responses:
+            200: [
+                {
+                    "id": <report_id>,
+                    "plan": <plan_id>,
+                    "uploaded_file": <file_url>,
+                    ...
+                },
+                ...
+            ]
+            404: {"error": "Plan not found" / "audit report not found"}
+        """
         plan = Plan.objects.filter(trace_code=trace_code).first()
         if not plan:
             return Response({'error': 'Plan not found'}, status=status.HTTP_404_NOT_FOUND)
@@ -119,6 +223,25 @@ class AuditReportViewset(APIView) :
 
     @method_decorator(ratelimit(key='ip', rate='5/m', method='DELETE', block=True))
     def delete (self,request,trace_code) :
+        """
+        Delete a specific audit report by ID.
+
+        This method authenticates the admin using the Authorization header and deletes a specific audit report
+        identified by its ID.
+
+        - If the Authorization header is missing, an error is returned.
+        - If the admin's authorization token is invalid or the admin is not found, an error is returned.
+        - If the audit report with the specified ID does not exist, an error is returned.
+
+        Parameters:
+            - Authorization (str, header): Admin's authorization token.
+            - trace_code (str, path): Unique identifier (ID) for the audit report to be deleted.
+
+        Responses:
+            200: {"message": "success"}
+            400: {"error": "Authorization header is missing"}
+            404: {"error": "admin not found" / "audit report not found"}
+        """
         Authorization = request.headers.get('Authorization')
         if not Authorization:
             return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
@@ -133,11 +256,37 @@ class AuditReportViewset(APIView) :
         return Response({'message':'succes'} , status=status.HTTP_200_OK)
 
 
-# گزارش مشارکت کننده ها
-# done
-class ParticipationReportViewset(APIView) :
+class ParticipationReportViewset(APIView):
+    """
+    This view allows a user to retrieve their participation report for a specific crowdfunding project, identified by the plan's trace code.
+    """
+
     @method_decorator(ratelimit(key='ip', rate='5/m', method='GET', block=True))
-    def get (self , request, trace_code) :
+    def get(self, request, trace_code):
+        """
+        Retrieve a participation report for a specific plan.
+
+        This method authenticates the user using the Authorization header and retrieves a participation report for a
+        specific plan identified by its trace code. The report is generated as a PDF file and stored on the server,
+        with a URL link returned in the response.
+
+        - If the Authorization header is missing, an error is returned.
+        - If the user's authorization token is invalid or the user is not found, an error is returned.
+        - If the plan with the specified trace code does not exist, an error is returned.
+        - If an error occurs during report generation, an error is returned with the corresponding status.
+
+        Parameters:
+            - Authorization (str, header): User's authorization token.
+            - trace_code (str, path): Unique trace code identifier for the plan.
+
+        Responses:
+            200: {
+                "url": "/media/reports/<trace_code>_<national_id>.pdf"
+            }
+            400: {"error": "Authorization header is missing"}
+            404: {"error": "user not found" / "plan not found"}
+            Other (pass-through): Error responses from CrowdfundingAPI service
+        """
         Authorization = request.headers.get('Authorization')
         if not Authorization:
             return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
@@ -161,11 +310,38 @@ class ParticipationReportViewset(APIView) :
         return Response({'url':f'/media/reports/{file_name}'},status=status.HTTP_200_OK)
 
 
-# داشبورد ادمین 
-# done
 class DashBoardAdminViewset (APIView) : 
-    @method_decorator(ratelimit(key='ip', rate='5/m', method='GET', block=True))
+    """
+    This view provides an admin with an overview dashboard that includes statistics on plans and carts.
+    """
+    @method_decorator(ratelimit(key='ip', rate='20/m', method='GET', block=True))
     def get (self , request) :
+        """
+        Retrieve dashboard statistics for plans and carts.
+
+        This method authenticates the admin using the Authorization header and returns a summary of
+        statistics related to plans and carts, including counts of all plans, expired plans, active plans, 
+        plans currently shown, all carts, expired carts, and active carts.
+
+        - If the Authorization header is missing, an error is returned.
+        - If the admin's authorization token is invalid or the admin is not found, an error is returned.
+
+        Parameters:
+            - Authorization (str, header): Admin's authorization token.
+
+        Responses:
+            200: {
+                "all plan": <total_plan_count>,
+                "expire plan": <expired_plan_count>,
+                "active plan": <active_plan_count>,
+                "show_plan": <show_plan_count>,
+                "all cart": <total_cart_count>,
+                "expire cart": <expired_cart_count>,
+                "active cart": <active_cart_count>
+            }
+            400: {"error": "Authorization header is missing"}
+            401: {"error": "admin not found"}
+        """
         Authorization = request.headers.get('Authorization')
         if not Authorization:
             return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
@@ -198,12 +374,50 @@ class DashBoardAdminViewset (APIView) :
         return Response (statics, status=status.HTTP_200_OK)
    
 
+class DashBoardUserViewset(APIView):
+    """
+    This view provides an overview dashboard for users, including statistics on plans, payments, total investment, 
+    rate of return, and profit dates.
+    """
 
-# داشبورد مشتری 
-# done
-class DashBoardUserViewset(APIView) :
     @method_decorator(ratelimit(key='ip', rate='5/m', method='GET', block=True))
-    def get (self,request) : 
+    def get(self, request):
+        """
+        Retrieve user-specific dashboard statistics.
+
+        This method authenticates the user using the Authorization header and returns a summary of statistics
+        specific to the user, including:
+        
+        - Total number of plans
+        - Number of active plans
+        - User's participation in plans
+        - Total value of user’s investments
+        - Total rate of return
+        - A list of profit dates, amounts, and types associated with the user’s participations
+
+        Parameters:
+            - Authorization (str, header): User's authorization token.
+
+        Responses:
+            200: {
+                "all plan": <total_plan_count>,
+                "active plan": <active_plan_count>,
+                "participant plan": <user_participation_count>,
+                "total value": <total_investment_value>,
+                "all rate of return": <total_rate_of_return>,
+                "profit": [
+                    {
+                        "type": <operation_type>,
+                        "date": <jalali_date>,
+                        "amount": <profit_amount>,
+                        "plan": <plan_id>
+                    },
+                    ...
+                ]
+            }
+            400: {"error": "Authorization header is missing"}
+            404: {"error": "user not found"}
+        """
         Authorization = request.headers.get('Authorization')
         if not Authorization:
             return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
@@ -240,10 +454,49 @@ class DashBoardUserViewset(APIView) :
             total_rate_of_return = 0
         return Response ({'all plan' :plan_all , 'active plan' : active_plan , 'participant plan' :payments_count , 'total value' : total_value , 'all rate of return' :  total_rate_of_return  , 'profit' : date_profit}, status=status.HTTP_200_OK)
     
-# گزارش سود دهی ادمین
-class ProfitabilityReportViewSet(APIView) :
+
+class ProfitabilityReportViewSet(APIView):
+    """
+    This view provides a profitability report for a specific plan, identified by its trace code.
+    """
+
     @method_decorator(ratelimit(key='ip', rate='5/m', method='GET', block=True))
-    def get(self,request,trace_code):
+    def get(self, request, trace_code):
+        """
+        Retrieve profitability report for a specific plan by trace code.
+
+        This method authenticates the admin using the Authorization header and generates a detailed profitability
+        report for a specific plan, including user payments, calculated profits, and payment dates. The report 
+        is based on the plan's rate of return and the project's end dates.
+
+        - If the Authorization header is missing, an error is returned.
+        - If the admin's authorization token is invalid or the admin is not found, an error is returned.
+        - If the trace code, plan, or related information is missing, an error is returned.
+        - If the plan has not ended, an error is returned.
+
+        Parameters:
+            - Authorization (str, header): Admin's authorization token.
+            - trace_code (str, path): Unique trace code identifier for the plan.
+
+        Responses:
+            200: [
+                {
+                    "user": <user_identifier>,
+                    "amount": <user_investment_amount>,
+                    "value": <user_investment_value>,
+                    "account_number": <user_account_number>,
+                    "user_name": <user_name>,
+                    "profit1": <profit1_value>,
+                    "value1": <value1_value>,
+                    "date_operator1": <date1>,
+                    ...
+                },
+                ...
+            ]
+            400: {"error": "Authorization header is missing" / "trace_code not found"}
+            404: {"error": "admin not found" / "plan not found"}
+            406: {"error": "plan not end" / "payment not found" / "information not found"}
+        """
         Authorization = request.headers.get('Authorization')
         if not Authorization:
             return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
